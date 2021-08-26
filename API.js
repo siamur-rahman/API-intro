17972509abdb8b300c76e32fb6b4b2eebf45551a
unknown <siamur01@gmail.com>
2021-08-26 17:01:03 +0600

    second commit

--- API.js
+++ API.js
@@ -14,10 +14,116 @@ function loadData() {
       .then(json => console.log(json))
 }*/
 
-
 /*
+
 function loadData() {
    fetch('https://jsonplaceholder.typicode.com/todos/1')
       .then(response => response.json())
       .then(data => console.log(data))
+}
+
+
+function loadUsers() {
+   fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => console.log(data));
+}
+function loadPost() {
+   fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(data => console.log(data));
+}
+// loadPost();
+
+*/
+
+/*
+function loadUsers() {
+   fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => displayUsers(data));
+}
+
+function displayUsers(data) {
+   console.log(data);
 }*/
+
+/*///shobgula user pete.........
+function loadUsers() {
+   fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => displayUsers(data));
+}
+
+function displayUsers(data) {
+   for (const user of data) {
+      console.log(user);
+   }
+   console.log(data);
+}
+*/
+/*
+// user theke sob nam pete............
+function loadUsers() {
+   fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => displayUsers(data));
+}
+
+function displayUsers(data) {
+   for (const user of data) {
+      console.log(user.name);
+   }
+}*/
+
+
+/*
+function loadUsers() {
+   fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => displayUsers(data));
+}
+
+function displayUsers(data) {
+   const ul = document.getElementById('users')
+   for (const user of data) {
+      console.log(user.name);
+      const li = document.createElement('li');
+      li.innerText = user.name;
+      ul.appendChild(li)
+   }
+}*/
+
+/*
+function loadUsers() {
+   fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => displayUsers(data));
+}
+
+function displayUsers(data) {
+   const ul = document.getElementById('users')
+   for (const user of data) {
+      console.log(user.name);
+      const li = document.createElement('li');
+      li.innerText = `name: ${user.name};`
+      ul.appendChild(li)
+   }
+}*/
+
+function loadUsers() {
+   fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => displayUsers(data));
+}
+
+function displayUsers(data) {
+   const ul = document.getElementById('users')
+   for (const user of data) {
+      console.log(user.name);
+      const li = document.createElement('li');
+      li.innerText = `name: ${user.name}   email: ${user.email}`;
+      ul.appendChild(li)
+   }
+}
+
